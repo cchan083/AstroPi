8fac83c5e30160245d1cce1d859c5b348287bb0e
Christopher Chan <ch.christopherchan@gmail.com>
2025-01-20 19:00:56 +0800

    Printing speed

--- iss_speed.py
+++ iss_speed.py
@@ -16,12 +16,12 @@ def get_time_difference(image_1, image_2):
     time_1 = get_time(image_1)
     time_2 = get_time(image_2)
     time_difference = time_2 - time_1
-    return time_difference.seconds
+    return time_difference.seconds  # Time difference
 
 
 def convert_to_cv(image_1, image_2):
     image_1_cv = cv2.imread(image_1, 0)
-    image_2_cv = cv2.imread(image_2, 0)
+    image_2_cv = cv2.imread(image_2, 0) # Making images the a CV2 object
     return image_1_cv, image_2_cv
 
 
@@ -33,31 +33,21 @@ def calculate_features(image_1, image_2, feature_number):
 
 
 def calculate_matches(descriptors_1, descriptors_2):
-    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True) #Brute force matching
     matches = brute_force.match(descriptors_1, descriptors_2)
     matches = sorted(matches, key=lambda x: x.distance)
-    return matches
+    return matches 
     
 
 def display_matches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches):
     match_img = cv2.drawMatches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches[:100], None)
     resize = cv2.resize(match_img, (1600,600), interpolation = cv2.INTER_AREA)
     cv2.imshow('matches', resize)
-    cv2.waitKey(0)
+    cv2.waitKey(0) #Drawing the matches 
     cv2.destroyWindow('matches')
     
     
-def find_matching_coordinates(keypoints_1, keypoints2, matches):
-    coordinates_1 = []
-    coordinates_2 = []
-    for match in matches:
-        image_1_idx = match.queryIdx
-        image_2_idx = match.trainIdx
-        (x1,y1) = keypoints_1[image_1_idx].pt
-        (x2,y2) = keypoints_2[image_2_idx].pt
-        coordinates_1.append((x1,y1))
-        coordinates_2.append((x2,y2))
-    return coordinates_1, coordinates_2
+
 
 
 def calculate_mean_distance(coordinates_1, coordinates_2):
@@ -66,14 +56,14 @@ def calculate_mean_distance(coordinates_1, coordinates_2):
     for coordinate in merged_coordinates:
         x_difference = coordinate[0][0] - coordinate[1][0]
         y_difference = coordinate[0][1] - coordinate[1][1]
-        distance = math.hypot(x_difference, y_difference)
+        distance = math.hypot(x_difference, y_difference) # Pythagoras for the distance 
         all_distances = all_distances + distance
     return all_distances / len(merged_coordinates)
 
 
 def calculate_speed_in_kmps(feature_distance, GSD, time_difference):
     distance = feature_distance * GSD / 100000
-    speed = distance / time_difference
+    speed = distance / time_difference 
     return speed
 
 def find_matching_coordinates(keypoints_1, keypoints_2, matches):
@@ -90,8 +80,8 @@ def find_matching_coordinates(keypoints_1, keypoints_2, matches):
         
 
 
-image_1 = 'photo_07464.jpg'
-image_2 = 'photo_07465.jpg'
+image_1 = 'photo_05515.jpg'
+image_2 = 'photo_05516.jpg'
 
 
 time_difference = get_time_difference(image_1, image_2) #get time difference between images
